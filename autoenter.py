68770be6305f39e148d29cafa86ccb8c22a71015
luudinhmac <nguoiemcuanui@gmail.com>
2023-06-27 16:11:31 +0700

    up git

--- autoenter.py
+++ autoenter.py
@@ -1,4 +1,5 @@
-import pyautogui, pyperclip, random, time
+import pyautogui, pyperclip, time
+from random import choice
 print("Tool Spam 1.0")
 msg1=["Xin lỗi","Cảm ơn"]
 msg = input("Nhập nội dung cần spam: ").split(" || ")
@@ -6,18 +7,17 @@ n = int(input("Nhập số lần Spam =)) "))
 m = float(input("Nhập thời gian delay: "))
 
 print("Chuẩn bị")
-#Đếm ngược 5 giây1
-
+#count reverse  5 second
 
 for i in range(5,0,-1):
- print(i,end="...",flush='True')
- time.sleep(1)
+    print(i,end="...",flush=True)
+    time.sleep(1)
 
 print("Bắt đầu")
 
 #SPAM
 for i in range(n):
- pyperclip.copy(random.choice(msg1))
+ pyperclip.copy(choice(msg1))
  pyautogui.hotkey("ctrl","v")
  pyautogui.press("enter")
  pyautogui.press("a")
